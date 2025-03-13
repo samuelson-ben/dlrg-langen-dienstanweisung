b6cfcbba85ed980a8989c918cc93bba2ce412505
FiTeKa <ben.samuelson2007@gmail.com>
2025-03-13 18:20:26 +0100

    fixed bug from chatgpt with chatgpt

--- delete_files.bat
+++ delete_files.bat
@@ -1,38 +1,28 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Verzeichnis, aus dem die Dateien gelöscht werden sollen
-set "TARGET_DIR=."
+:: Check if keeppdf is passed as a parameter
+set keeppdf=false
+if "%1"=="keeppdf" set keeppdf=true
 
-:: Datei mit der Liste der zu löschenden Dateien
-set "LIST_FILE=delete_list.txt"
+:: Open delete_list.txt and read each line
+for /f "delims=" %%F in (delete_list.txt) do (
+    set filename=%%F
 
-:: Überprüfen, ob die Liste existiert
-if not exist "%LIST_FILE%" (
-    echo Die Datei %LIST_FILE% existiert nicht!
-    exit /b
-)
-
-:: Prüfen, ob das Argument "keeppdf" übergeben wurde
-set "KEEP_PDF=false"
-if /I "%1"=="keeppdf" set "KEEP_PDF=true"
-
-:: Dateien aus der Liste löschen
-for /f "delims=" %%f in (%LIST_FILE%) do (
-    set "FILE_PATH=%TARGET_DIR%\%%f"
-    if /I "!KEEP_PDF!"=="true" if /I "%%~xf"==".pdf" (
-        echo Übersprungen (PDF): %%f
-        goto :continue
-    )
-    
-    if exist "!FILE_PATH!" (
-        del "!FILE_PATH!"
-        echo Gelöscht: %%f
+    :: Check if the file extension is .pdf and keeppdf is true
+    if !keeppdf! == true (
+        echo Checking file: !filename!
+        set extension=!filename:~-4!
+        if /i "!extension!"==".pdf" (
+            echo Skipping PDF file: !filename!
+        ) else (
+            echo Deleting file: !filename!
+            del "%%F"
+        )
     ) else (
-        echo Nicht gefunden: %%f
+        echo Deleting file: !filename!
+        del "%%F"
     )
-    
-    :continue
 )
 
 endlocal
