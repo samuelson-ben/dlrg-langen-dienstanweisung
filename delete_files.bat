fdb4811326672045a606364cac5bdc482bf93481
FiTeKa <ben.samuelson2007@gmail.com>
2025-03-13 10:35:13 +0100

    autofunction for windows

---
+++ delete_files.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Verzeichnis, aus dem die Dateien gelöscht werden sollen
+set "TARGET_DIR=."
+
+:: Datei mit der Liste der zu löschenden Dateien
+set "LIST_FILE=delete_list.txt"
+
+:: Überprüfen, ob die Liste existiert
+if not exist "%LIST_FILE%" (
+    echo Die Datei %LIST_FILE% existiert nicht!
+    exit /b
+)
+
+:: Dateien aus der Liste löschen
+for /f "delims=" %%f in (%LIST_FILE%) do (
+    set "FILE_PATH=%TARGET_DIR%\%%f"
+    if exist "!FILE_PATH!" (
+        del "!FILE_PATH!"
+        echo Gelöscht: %%f
+    ) else (
+        echo Nicht gefunden: %%f
+    )
+)
