a5b2f9b8a0f04872c60a6e88290eee4ade5cafc7
FiTeKa <ben.samuelson2007@gmail.com>
2025-03-13 13:29:50 +0100

    added keeppdf arg for windows

--- delete_files.bat
+++ delete_files.bat
@@ -13,13 +13,26 @@ if not exist "%LIST_FILE%" (
     exit /b
 )
 
+:: Prüfen, ob das Argument "keeppdf" übergeben wurde
+set "KEEP_PDF=false"
+if /I "%1"=="keeppdf" set "KEEP_PDF=true"
+
 :: Dateien aus der Liste löschen
 for /f "delims=" %%f in (%LIST_FILE%) do (
     set "FILE_PATH=%TARGET_DIR%\%%f"
+    if /I "!KEEP_PDF!"=="true" if /I "%%~xf"==".pdf" (
+        echo Übersprungen (PDF): %%f
+        goto :continue
+    )
+    
     if exist "!FILE_PATH!" (
         del "!FILE_PATH!"
         echo Gelöscht: %%f
     ) else (
         echo Nicht gefunden: %%f
     )
+    
+    :continue
 )
+
+endlocal
